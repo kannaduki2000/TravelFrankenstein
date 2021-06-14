45791b8c2735d0fa17ebfba1fd24887e04332ca8
ToriumiHonami <asahiiii0725@gmail.com>
2021-06-14 22:01:25 +0900

    物を持ち上げる、投げる処理

--- Frankenstein/Assets/WorkFolder/Toriumi/Scripts/Item.cs
+++ Frankenstein/Assets/WorkFolder/Toriumi/Scripts/Item.cs
@@ -16,111 +16,48 @@ public class Item : MonoBehaviour
         
     }
 
+    //低く投げる
     public void Low()
     {
-        ////力を加える向き
-        //Vector3 forceDirection = new Vector3(3, 3, 0);
+        //x軸方向
+        float I_speed  = 70f;
+        float I_degree = 60f; // 60°
 
-        ////上の向きに加わる力の大きさ
-        //float forceMagnitude = 10.0f;
-
-        ////向きと大きさからitemに加わる力を計算
-        //Vector3 force = forceMagnitude * forceDirection;
-
-        ////itemオブジェクトのrigidbodyコンポーネントへの参照
-        //Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-
-        ////力を加えるメソッド
-        //rb.AddForce(force, ForceMode2D.Impulse);
-
-        float I_speed = 60f;
-        float I_degree = 60f;
+        //y軸方向
+        float I_Speed  = 60f;
+        float I_Degree = 45f; // 45°
 
         Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
 
         Vector3 vel = Vector3.zero;
-        vel.y = I_speed;
+       
+
+        //memo 『Mathf』三角関数の定数とメソッドを提供
+        //memo2『PI』   πを指定
+        //memo3 I_degreeπ/180 = 1/x = x°
+        //memo4 x軸方向の計算 v0cosθ * t
+        //memo5 y軸方向の計算 v0sinθ - gt
 
         vel.x = I_speed * Mathf.Cos(I_degree * Mathf.PI / 180f);
-        vel.y = I_speed * Mathf.Sin(I_degree * Mathf.PI / 180f);
+        vel.y = I_Speed * Mathf.Sin(I_Degree * Mathf.PI / 180f);
         rb.velocity = vel;
-        this.transform.parent = null;
-
     }
 
+    //高く投げる
     public void Hight()
     {
-        ////力を加える向き
-        //Vector3 forceDirection = new Vector3(6, 6, 0);
-
-        ////上の向きに加わる力の大きさ
-        //float forceMagnitude = 10.0f;
-
-        ////向きと大きさからitemに加わる力を計算
-        //Vector3 force = forceMagnitude * forceDirection;
-
-        ////itemオブジェクトのrigidbodyコンポーネントへの参照
-        //Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-
-        ////力を加えるメソッド
-        //rb.AddForce(force, ForceMode2D.Impulse);
-
-        float I_speed = 80f;
-        float I_degree = 80f;
+        float I_speed  = 55f;
+        float I_degree = 60f;　// 60°
+        float I_Speed  = 70f;
+        float I_Degree = 60f;　// 60°
 
         Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
 
         Vector3 vel = Vector3.zero;
-        vel.y = I_speed;
+        
 
-        vel.x = I_speed * Mathf.Cos(I_degree * Mathf.PI / 192.5f);
-        vel.y = I_speed * Mathf.Sin(I_degree * Mathf.PI / 192.5f);
+        vel.x = I_speed * Mathf.Cos(I_degree * Mathf.PI / 180f);
+        vel.y = I_Speed * Mathf.Sin(I_Degree * Mathf.PI / 180f);
         rb.velocity = vel;
-        this.transform.parent = null;
     }
-
-    //public void Move()
-    //{
-    //    Vector3 pos = transform.position;
-
-    //    //y軸
-    //    pos.y = 50;
-
-    //    //x軸
-    //    PlayerControl player = gameObject.GetComponent<PlayerControl>();
-
-    //    // z
-    //    pos.z = 0;
-        
-        
-    //    if (Input.GetKey(KeyCode.A))
-    //    {
-    //        pos.x -= 2;
-    //    }
-    //    //右矢印キーを押している
-    //    else if (Input.GetKey(KeyCode.D))
-    //    {
-    //        pos.x += 2;
-    //    }
-    //    float x = Input.GetAxisRaw("Horizontal");
-
-    //    GameObject obj = GameObject.Find("Player");
-
-    //    Vector3 scale = obj.transform.localScale;
-
-    //    if (x > 0)
-    //    {
-    //        scale.x = 1;
-
-    //    }
-    //    else if (x < 0)
-    //    {
-    //        scale.x = -1;
-    //    }
-
-    //    obj.transform.localScale = scale;
-    //    transform.position = pos;
-    //    //Debug.Log("〇");
-
-    //}
 }
