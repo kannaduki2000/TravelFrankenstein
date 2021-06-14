ec5d1982d5f1b78c40c518b4d00ecef35ec58fb4
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2021-06-14 17:25:24 +0900

    デバッグの説明文追加　Assembly参照不具合でBuildErrorがでた場合はデバッグのゲームオブジェクトを削除してみてください

---
+++ Frankenstein/Assets/WorkFolder/kumakura/Scripts/Editor/GameSceneDebugEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CustomEditor(typeof(GameSceneDebug))]
+public class GameSceneDebugEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.HelpBox(
+        "DebugMode\n" +
+        "true : デバッグモードの有効化\n" +
+        "false : デバッグモードの無効化\n\n", MessageType.Info);
+        EditorGUILayout.HelpBox(
+        "～使い方～\n" +
+        "左Ctrlキー : シーンの再読み込み\n" +
+        "左Shiftキー : ゲーム内時間3倍に変更\n" +
+        "右Shiftキー : ゲーム内時間0.5倍に変更\n" +
+        "Shiftキーを離すと通常のゲーム内時間に戻ります", MessageType.Info);
+        EditorGUILayout.HelpBox("BuildErrorが起こる場合はこのGameObjectを破棄してください", MessageType.Warning);
+    }
+}
