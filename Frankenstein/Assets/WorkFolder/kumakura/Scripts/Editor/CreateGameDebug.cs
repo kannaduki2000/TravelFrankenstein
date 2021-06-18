d7b5b8c690a18539afcf777d4f15f49be376e152
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2021-06-18 09:45:20 +0900

    いったんあげ

---
+++ Frankenstein/Assets/WorkFolder/kumakura/Scripts/Editor/CreateGameDebug.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateGameDebug : MonoBehaviour
+{
+
+    [MenuItem("Frankenstein/Create GameDebug")]
+    public static
+}
