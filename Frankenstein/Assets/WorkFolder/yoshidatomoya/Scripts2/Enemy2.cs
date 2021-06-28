ae93ce4cc2dad3192cb48531a4ace44f2745a126
MYCOMPUTER\tomoy <tomoya5960@gmail.com>
2021-06-29 04:51:58 +0900

    エネミーの行動　振り向き追加

--- Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts2/Enemy2.cs
+++ Frankenstein/Assets/WorkFolder/yoshidatomoya/Scripts2/Enemy2.cs
@@ -22,29 +22,37 @@ public class Enemy2 : MonoBehaviour
     private float TimeRimit = 2.0f;
     private bool move = false;
 
-    float time = 0f;
+    private float time = 0f;
 
-    private void FixedUpdate() // ずっと、往復する
-    {
-        if(move == true)
+    // ずっと、往復する
+    void FixedUpdate() 
+    { 
+        time += Time.deltaTime;
+        float s = Mathf.Sin(Time.time); // 移動量を求める
+        this.transform.Translate(speedX * s / 50, speedY * s / 50, speedZ * s / 50);
+        // デフォルトが右向きの画像の場合
+        // スケール値取り出し
+        Vector3 scale = transform.localScale;
+        if (s >= 0)
         {
-            time += Time.deltaTime;
-            float s = Mathf.Sin(time * 3.14f / second); // 移動量を求める
-            this.transform.Translate(speedX * s / 50, speedY * s / 50, speedZ * s / 50);
+            // 右方向に移動中
+            scale.x = 1; // そのまま（右向き）
         }
+        else
+        {
+            // 左方向に移動中
+            scale.x = -1; // 反転する（左向き）
+        }
+        // 代入し直す
+        transform.localScale = scale;
 
-        
     }
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // プレイヤータグの取得
-       // Player = GameObject.FindWithTag("Player");
-        //Target = Player.transform;
-
-        //Controller = GetComponent<CharacterController>();
 
 
     }
@@ -52,7 +60,7 @@ public class Enemy2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
         if (move == false)
         {
             UpdateTimer += Time.deltaTime;
@@ -62,54 +70,11 @@ public class Enemy2 : MonoBehaviour
             move = true;
             UpdateTimer = 0;
         }
-        
-
-
-        /*
-        if(InArea)
-        {
-            Debug.Log("abc");
-            // プレイヤーのほうを向かせる
-            this.transform.LookAt(Target.transform);
-
-            // キューブとプレイヤー間の距離を計算
-            Vector3 direction = Target.position - this.transform.position;
-            direction = direction.normalized;
-
-            // プレイヤー方向の速度を作成
-            Vector3 velocity = direction * MoveSpeed;
-
-            // プレイヤーがジャンプした時にキューブが浮かないようにy速度を0に固定しておく
-            velocity.y = 0.0f;
-
-            // キューブを動かす
-            Debug.Log("nnn");
-            Controller.Move(velocity * Time.deltaTime);
-        }
         */
-        // プレイヤーとキューブ間の距離を計算
-        //Vector3 Apos = this.transform.position;
-        //Vector3 Bpos = Target.transform.position;
-        //float distance = Vector3.Distance(Apos, Bpos);
 
-        /*
-        // 距離がDetecDistの設定値未満の場合は検知フラグをfalseにする
-        if(distance > DetecDist)
-        {
-            Debug.Log("aaa");
-            InArea = false;
-        }
-        */
     }
 
-    /*
-    // プレイヤーが検知エリアにいたら検知フラグをtrueにする
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("動いた！");
-        InArea = true;
-    }
-    */
+
 }
 
 // エネミー自身のテリトリー間を行き来する〇
@@ -121,3 +86,55 @@ public class Enemy2 : MonoBehaviour
 // 突進した場所にフランケンがいなかったら２秒間待機して自分のテリトリーに戻る
 // 
 
+// プレイヤータグの取得
+// Player = GameObject.FindWithTag("Player");
+//Target = Player.transform;
+
+//Controller = GetComponent<CharacterController>();
+
+/*
+       if(InArea)
+       {
+           Debug.Log("abc");
+           // プレイヤーのほうを向かせる
+           this.transform.LookAt(Target.transform);
+
+           // キューブとプレイヤー間の距離を計算
+           Vector3 direction = Target.position - this.transform.position;
+           direction = direction.normalized;
+
+           // プレイヤー方向の速度を作成
+           Vector3 velocity = direction * MoveSpeed;
+
+           // プレイヤーがジャンプした時にキューブが浮かないようにy速度を0に固定しておく
+           velocity.y = 0.0f;
+
+           // キューブを動かす
+           Debug.Log("nnn");
+           Controller.Move(velocity * Time.deltaTime);
+       }
+*/
+
+// プレイヤーとキューブ間の距離を計算
+//Vector3 Apos = this.transform.position;
+//Vector3 Bpos = Target.transform.position;
+//float distance = Vector3.Distance(Apos, Bpos);
+
+
+/*
+// 距離がDetecDistの設定値未満の場合は検知フラグをfalseにする
+if(distance > DetecDist)
+{
+    Debug.Log("aaa");
+    InArea = false;
+}
+*/
+
+/*
+// プレイヤーが検知エリアにいたら検知フラグをtrueにする
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    Debug.Log("動いた！");
+    InArea = true;
+}
+*/
