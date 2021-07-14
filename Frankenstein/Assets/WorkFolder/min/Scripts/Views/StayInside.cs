e145fb82459a4a15ac4b2ad4ef1a7416b614ba75
minhtet97 <minhtetnai97@gmail.com>
2021-07-14 14:12:42 +0900

    マジックナンバーから変数に変更しました。

--- Frankenstein/Assets/WorkFolder/min/Scripts/Views/StayInside.cs
+++ Frankenstein/Assets/WorkFolder/min/Scripts/Views/StayInside.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class StayInside : MonoBehaviour
 {
+    [SerializeField]
+    private float LeftX = -53f;
+    private float RightX = 128f;
+    private float UpY = -100f;
+    private float DownY = 21f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -53f, 128f),
-            Mathf.Clamp(transform.position.y, -100f, 21f), transform.position.z); 
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, LeftX, RightX),
+            Mathf.Clamp(transform.position.y, UpY, DownY), transform.position.z); 
     }
 }
