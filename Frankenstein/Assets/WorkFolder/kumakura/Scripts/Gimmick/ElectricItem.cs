0789de4bc374c8b429d11b7333e6a3f8ae8a7b21
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2021-07-21 11:25:07 +0900

    電気Objectの基底クラス編集

--- Frankenstein/Assets/WorkFolder/kumakura/Scripts/Gimmick/ElectricItem.cs
+++ Frankenstein/Assets/WorkFolder/kumakura/Scripts/Gimmick/ElectricItem.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 public class ElectricItem : MonoBehaviour
 {
     public int Power { get; set; } = 0;                 // 消費電力
-    public bool ChargeFlag { get; set; } = false;       // 電気を入れる/充電　OnOff可能
+    public bool ChargeFlag { get; set; } = false;       // 電気を入れてあるかどうか　OnOff可能
     public bool IsElectricEvent { get; set; } = false;  // Event中かどうか
     public bool IsChargeEvent { get; set; } = false;    // Event実行済かどうか　Onのみ
-    public Image AnnounceImage { get; set; } = null;    // 指示表示画像
+    public Sprite[] AnnounceImage { get; set; } = null;  // 指示表示画像
 
     public virtual void Event() { }
 
