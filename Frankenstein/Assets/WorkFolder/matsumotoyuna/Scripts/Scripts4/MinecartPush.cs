a0cfca1a11f80d0e3a29c9cf9ea651b4015b9bd5
home-PC\Ringo Mochi <tech@t>
2021-07-30 14:56:39 +0900

    お試しプッシュ、ほとんど何も追加してないよ

---
+++ Frankenstein/Assets/WorkFolder/matsumotoyuna/Scripts/Scripts4/MinecartPush.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//エネミーがトロッコの前でボタンを押すと、
+//トロッコが動く仕組みのスクリプトだよ！
+public class MinecartPush : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
