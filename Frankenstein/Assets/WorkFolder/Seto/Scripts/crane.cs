3dd73aa188c2c10e4999f280005522132d59854f
DaikiSeto0917 <daiki.0998.cu@gmail.com>
2021-08-30 13:23:25 +0900

    クレーン完成これで大丈夫だと思います。

---
+++ Frankenstein/Assets/WorkFolder/Seto/Scripts/crane.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crane : MonoBehaviour
+{
+    [SerializeField] GameObject enem;
+    [SerializeField] GameObject enem1;
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        AnimationControl();
+    }
+      private void AnimationControl()
+    {
+        //"e"キーを押したときの処理
+        if(Input.GetKeyDown("e"))
+        {
+            //エネミーを非アクティブ
+            enem.SetActive(false);
+            //エネミー1をアクティブ
+            enem1.SetActive(true);
+            //アニメーションCraneを再生
+            anim.Play("Crane");
+        }
+    }
+}
