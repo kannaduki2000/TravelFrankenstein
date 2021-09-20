7ee3164b9c2e3d63cd7511d6b2c76dac8f2143d3
KumakuraToyotsugu <tokkun.nga-.0423@ezweb.ne.jp>
2021-09-20 21:19:57 +0900

    デバッグモードの激アツ更新

--- Frankenstein/Assets/WorkFolder/kumakura/Scripts/Debug/GameSceneDebug.cs
+++ Frankenstein/Assets/WorkFolder/kumakura/Scripts/Debug/GameSceneDebug.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameSceneDebug : MonoBehaviour
 {
     [SerializeField] private bool debugMode = false;
     private PlayerController player = null;
     private TextController text = null;
+    private FadeControl fade;
 
 
     void Start()
@@ -20,6 +24,8 @@ public class GameSceneDebug : MonoBehaviour
         {
             text = FindObjectOfType<TextController>();
         }
+
+        fade = FindObjectOfType<FadeControl>();
     }
 
     void Update()
@@ -72,5 +78,66 @@ public class GameSceneDebug : MonoBehaviour
             Time.timeScale = 1;
         }
         #endif
+    }
+
+    private void GetUp()
+    {
+        EventFlagManager.Instance.SetFlagState(EventFlagName.frankensteinGetUp, true);
+        EventFlagManager.Instance.SetFlagState(EventFlagName.getupFlag, true);
+    }
+
+    public void ChangeMap1()
+    {
+        GetUp();
+        fade.Fade("out", ()=> fade.sceneChange.SceneSwitching("MainStage_1"));
+    }
+
+    public void ChangeMap2()
+    {
+        GetUp();
+        fade.Fade("out", ()=> fade.sceneChange.SceneSwitching("Main_Stag2"));
+    }
+
+}
+
+
+[CustomEditor(typeof(GameSceneDebug))]
+public class GameSceneDebugEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.HelpBox(
+        "DebugMode\n" +
+        "true : デバッグモードの有効化\n" +
+        "false : デバッグモードの無効化\n\n", MessageType.Info);
+        EditorGUILayout.HelpBox(
+        "～使い方～\n" +
+        "左Ctrlキー : シーンの再読み込み\n" +
+        "左Shiftキー : ゲーム内時間3倍に変更\n" +
+        "右Shiftキー : ゲーム内時間0.5倍に変更\n" +
+        "Shiftキーを離すと通常のゲーム内時間に戻ります\n" +
+        "左Tabキー +\n" +
+        "　　　Tキー : テキストの表示速度最速化\n" +
+        "　　　Pキー : プレイヤーの移動速度上昇", MessageType.Info);
+        EditorGUILayout.HelpBox(
+        "Button\n" +
+        "ゲーム再生中に下のボタンを押すことで指定のシーンに飛べます", MessageType.Info);
+
+
+        GameSceneDebug gameSceneDebug = target as GameSceneDebug;
+
+        if (GUILayout.Button("Map1"))
+        {
+            gameSceneDebug.ChangeMap1();
+        }
+
+        if (GUILayout.Button("Map2"))
+        {
+            gameSceneDebug.ChangeMap2();
+        }
+
+
+        EditorGUILayout.HelpBox("BuildErrorが起こる場合はこのGameObjectを破棄してください", MessageType.Warning);
     }
 }
--- Frankenstein/Assets/WorkFolder/kumakura/Scripts/Editor/GameSceneDebugEditor.cs
+++ Frankenstein/Assets/WorkFolder/kumakura/Scripts/Editor/GameSceneDebugEditor.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-[CustomEditor(typeof(GameSceneDebug))]
-public class GameSceneDebugEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        EditorGUILayout.HelpBox(
-        "DebugMode\n" +
-        "true : デバッグモードの有効化\n" +
-        "false : デバッグモードの無効化\n\n", MessageType.Info);
-        EditorGUILayout.HelpBox(
-        "～使い方～\n" +
-        "左Ctrlキー : シーンの再読み込み\n" +
-        "左Shiftキー : ゲーム内時間3倍に変更\n" +
-        "右Shiftキー : ゲーム内時間0.5倍に変更\n" +
-        "Shiftキーを離すと通常のゲーム内時間に戻ります\n" +
-        "左Tabキー +\n" +
-        "　　　Tキー : テキストの表示速度最速化\n" +
-        "　　　Pキー : プレイヤーの移動速度上昇", MessageType.Info);
-        EditorGUILayout.HelpBox("BuildErrorが起こる場合はこのGameObjectを破棄してください", MessageType.Warning);
-    }
-}
+//[CustomEditor(typeof(GameSceneDebug))]
+//public class GameSceneDebugEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        EditorGUILayout.HelpBox(
+//        "DebugMode\n" +
+//        "true : デバッグモードの有効化\n" +
+//        "false : デバッグモードの無効化\n\n", MessageType.Info);
+//        EditorGUILayout.HelpBox(
+//        "～使い方～\n" +
+//        "左Ctrlキー : シーンの再読み込み\n" +
+//        "左Shiftキー : ゲーム内時間3倍に変更\n" +
+//        "右Shiftキー : ゲーム内時間0.5倍に変更\n" +
+//        "Shiftキーを離すと通常のゲーム内時間に戻ります\n" +
+//        "左Tabキー +\n" +
+//        "　　　Tキー : テキストの表示速度最速化\n" +
+//        "　　　Pキー : プレイヤーの移動速度上昇", MessageType.Info);
+
+
+//        GameSceneDebug gameSceneDebug = target as GameSceneDebug;
+//        if (GUILayout.Button("TutorialFlag"))
+//        {
+            
+//        }
+
+
+//        EditorGUILayout.HelpBox("BuildErrorが起こる場合はこのGameObjectを破棄してください", MessageType.Warning);
+//    }
+//}
